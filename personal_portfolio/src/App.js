5388d41c1d4344e9f7636ad0e4f1f746fde5fef8
Bairon29 <baironvasquez29@gmail.com>
2019-01-28 13:07:50 -0500

    Created start-up files, clean extra code, and set up template env to start working

--- personal_portfolio/src/App.js
+++ personal_portfolio/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import SelfIntro from './components/SelfIntro'
+import MainSkills from './components/MainSkills'
+import Background from './components/Background'
+import Education from './components/Education'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="intro-section">
+          <SelfIntro />
+        </section>
+        <section className="main-info-session">
+          <MainSkills />
+          <Background />
+          <Education />
+        </section>
       </div>
     );
   }
