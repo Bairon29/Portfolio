c0a98f1e0c18f51ee06345e63c88a635ebb3b84a
Bairon29 <baironvasquez29@gmail.com>
2019-01-28 14:34:04 -0500

    Set up HTML for selfIntro section

--- personal_portfolio/src/components/SelfIntro.js
+++ personal_portfolio/src/components/SelfIntro.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import me from './images/me.jpg'
 
 class SelfIntro extends Component {
   render() {
     return (
       <div className="SelfIntro">
+      	<div className="img-container">
+      		<img src={me} alt="Professional" />
+      	</div>
+      	<div className="title">
+      		<h1>Bairon J. Vasquez</h1>
+      		<h3>Software Engineer</h3>
+      	</div>
+      	<div className="contact-info">
+      		<div className="spe-contact-info">
+      			<span>Email:</span>
+      			<p>baironvasquez29@gmail.com</p>
+      		</div>
+      		<div className="spe-contact-info">
+      			<span>LinkedIn:</span>
+      			<a href="https://www.linkedin.com/in/bairon-jvasquez/" >LinkedIn Account</a>
+      		</div>
+      	</div>
       </div>
     );
   }
