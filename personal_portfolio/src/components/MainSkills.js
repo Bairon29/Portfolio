8ea96f5c401e2ed6e05a2bb76fdaacd7e901fb47
Bairon29 <baironvasquez29@gmail.com>
2019-01-28 13:17:42 -0500

    .DS_Store banished!

---
+++ personal_portfolio/src/components/MainSkills.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MainSkills extends Component {
+  render() {
+    return (
+      <div className="MainSkills">
+      </div>
+    );
+  }
+}
+
+export default MainSkills;
---
+++ personal_portfolio/src/components/SelfIntro.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SelfIntro extends Component {
+  render() {
+    return (
+      <div className="SelfIntro">
+      </div>
+    );
+  }
+}
+
+export default SelfIntro;
