98d70ea67873d79cbe21774e179f6980954d6f68
Bairon29 <baironvasquez29@gmail.com>
2019-01-31 11:53:33 -0500

    Starting Skill set

--- personal_portfolio/src/components/MainSkills.js
+++ personal_portfolio/src/components/MainSkills.js
@@ -4,6 +4,70 @@ class MainSkills extends Component {
   render() {
     return (
       <div className="MainSkills">
+      	<div className="skill-container">
+	      	<h1>PROGRAMMING SKILLS</h1>
+	      	<div className="skill" >
+	      		<h3>HTML5</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+	      	<div className="skill" >
+	      		<h3>CSS3</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+	      	<div className="skill" >
+	      		<h3>Javascript</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+	      	<div className="skill" >
+	      		<h3>NodeJS</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+	      	<div className="skill" >
+	      		<h3>Java</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+	      	<div className="skill" >
+	      		<h3>MongoDB</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+	      	<div className="skill" >
+	      		<h3>Oracle</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+      	</div>
+
+      	<div className="skill-container">
+	      	<h1>FRAMEWORKS AND OTHER TECHNOLOGIES</h1>
+	      	<div className="skill" >
+	      		<h3>HTML5</h3>
+	      		<div className="skill-scale-box">
+	      			<div className="skill-scale-color">
+	      			</div>
+	      		</div>
+	      	</div>
+      	</div>
+
       </div>
     );
   }
