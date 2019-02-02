adb978ccfb3c162c698173bfb3cb9b24455d2808
Bairon29 <baironvasquez29@gmail.com>
2019-02-02 14:06:38 -0500

    Bug: Fixed; SeflIntro HTML re-structure

--- personal_portfolio/src/App.js
+++ personal_portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './components/css/App.css';
 import './components/css/max-736px.css';
-import './components/css/max-1390px.css';
+import './components/css/max-1048px.css';
 
 import SelfIntro from './components/SelfIntro'
 import MainSkills from './components/MainSkills'
--- personal_portfolio/src/components/SelfIntro.js
+++ personal_portfolio/src/components/SelfIntro.js
@@ -5,14 +5,14 @@ class SelfIntro extends Component {
   render() {
     return (
       <div className="SelfIntro">
-      	<div className="img-container">
-      		<img src={me} alt="Professional" />
-      	</div>
-      	<div className="title">
-      		<h1>Bairon J. Vasquez</h1>
-      		<h3>Software Engineer</h3>
-      	</div>
-      	<div className="contact-info">
+				<div className="contact-info">
+					<div className="img-container">
+						<img src={me} alt="Professional" />
+					</div>
+					<div className="title">
+						<h1>Bairon J. Vasquez</h1>
+						<h3>Software Engineer</h3>
+					</div>
       		<table className="table-contact-info">
       			<tbody>
 	      			<tr>
@@ -25,13 +25,13 @@ class SelfIntro extends Component {
 	      			</tr>
       			</tbody>
       		</table>
-      		<div className="spe-contact-info">
-      			<p>
-							{/* boi */}
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-      		</div>
-      	</div>
+				</div>
+				<div className="spe-contact-info">
+					<p>
+						{/* bio */}
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+				</div>
       </div>
     );
   }
